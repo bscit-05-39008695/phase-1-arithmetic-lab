8418f52874b9e89fc391f5664deac1f6f08bb4b3
bscit-05-39008695 <aronmutai99@gmail.com>
2024-09-26 15:40:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // multiply should equal 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; // generates a random number greater than 0
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // mod should equal 4
+
+let max = Math.max(1, 2, 3, 20); // max should return 20
